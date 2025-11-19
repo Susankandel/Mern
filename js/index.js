e79819fa00effbad936fb9854efd0ffb6a939d8e
Susankandel <susankandel99@gmailcom>
2025-11-19 18:37:17 +0545

    learned about statements

--- js/index.js
+++ js/index.js
@@ -1,10 +1,20 @@
-console.log("Hello, Mern Stack!");
+// console.log("Hello, Mern Stack!");
 
-const name = "susan kandel";
-console.log(name);
+// const name = "susan kandel";
+// console.log(name);
 
-let person1 = "sadip";
-console.log(person1);
+// let person1 = "sadip";
+// console.log(person1);
 
-var person2 = "ssk";
-console.log(person2);
+// var person2 = "ssk";
+// console.log(person2);
+
+
+// console.log("HELLO WORLD")
+
+// const person1 = "SUSAN";
+// console.log("SUSAN");
+
+
+const name3 = prompt("what is your name");
+alert(`welcome back, ${name3}`);
---
+++ js/statement.js
@@ -0,0 +1,30 @@
+// if else
+
+// const gender = prompt ("enter your gender");
+// if (gender === "m"){
+//     console.log("male")
+// }
+// else{
+//     console.log("female")
+// }
+
+// ternaru operater (ES6)
+// const gender = prompt("enter your gender");
+// gender === "m" ? alert("male") : alert("female");
+
+//write a js program to ask user their drinks order
+// ("tea, coffe , milk, water,coke")
+// alert => you have ordered...
+
+// const drinks = prompt("Order a drinks");
+// drinks === "tea"
+//   ? alert("you have ordered tea")
+//   : drinks === "coffee"
+//   ? alert("you have ordered coffee")
+//   : drinks === "milk"
+//   ? alert("you have ordered milk")
+//   : drinks === "water"
+//   ? alert("you have ordered water")
+//   : drinks === "coke"
+//   ? alert("you have ordered coke")
+//   : alert("your order is invalid.");
